54e32c59966aa7a971446fc9d793c912cd7bd148
Lucas Couto <lucascouto91@gmail.com>
2025-04-11 13:49:01 -0300

    feat: CRUD Parte 1

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,7 +1,15 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunoForm {
 
   private String nome;
